fc1bb8fbc1f7064012940939ad990c36b5144217
Fursum <fursum0@gmail.com>
2021-10-08 02:17:52 +0300

    Better Turkish placeholder text
    using english text as placeholder instead

--- localization/pages/contact.ts
+++ localization/pages/contact.ts
@@ -30,22 +30,25 @@ const ContactLocalization = {
       {
         title: "Fikir",
         content:
-          "Bring some fun to your work with warm colors and cool layouts",
+          "Every website starts with an idea. Write down what you want: Colors, shapes, emotions... Then tell me about them!\n\nThe more I have for reference, the more accurate the website will be for your needs.\n\nYou may have the design ready, in that case we can skip some steps.",
       },
       {
         title: "Tasarım",
-        content: "Bring more depth to your blog with your tastes",
+        content:
+          "It is time to start doing prototypes. This is when the requirements should be decided.\n\nAt this stage I will be sending you some page mockups for feedback. Once you are happy with the site, we will move on to the next stage.\n\nThis is also the stage where the cost can be approximated.",
       },
       {
         title: "Geliştirme",
-        content: "Polish your storefront with modern and suitable designs",
+        content:
+          "The site is starting to take form. The mockups will become interactable, you will be able to start using the site.\n\nDon't worry, we are able to make adjustments still!",
       },
       {
         title: "Yayımlama",
-        content: "Polish your storefront with modern and suitable designs",
+        content:
+          "Now that the website is ready, it is time to share it with the world!\n\nAfter the necessary optimizations are made, such as SEO and accessibility features, we will put it up on a domain and start using it.",
       },
     ],
-    process: "How does the process work?",
+    process: "Süreç nasıl işliyor?",
   },
 };
 
